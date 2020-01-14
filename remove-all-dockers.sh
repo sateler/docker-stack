9c0185ac74c9b7a70ed21bf8f498a5e5aa59f501
Ricardo Sateler <rsateler@gmail.com>
2020-01-14 11:33:29 -0300

    Better start and remove scripts

--- remove-all-dockers.sh
+++ remove-all-dockers.sh
@@ -2,4 +2,8 @@
 
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
+docker rmi $(docker images -aq)
+docker rmi $(docker images --format "{{.Repository}}")
+docker rmi $(docker images -aq)
+docker network prune -f
+docker volume prune
--- start-dockers.sh
+++ start-dockers.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+docker network create proxy
 docker-compose -f traefik/docker-compose.yml up -d
 docker-compose -f phpmyadmin/docker-compose.yml up -d
