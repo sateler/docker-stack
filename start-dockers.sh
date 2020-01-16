e622199fc059317efab7b9753fda0e2398e8943a
Ricardo Sateler <rsateler@gmail.com>
2020-01-16 18:35:41 -0300

    Add mysql database to docker stack

--- start-dockers.sh
+++ start-dockers.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 docker network create proxy
+docker network create db
 docker-compose -f traefik/docker-compose.yml up -d
+docker-compose -f mysql/docker-compose.yml up -d
 docker-compose -f phpmyadmin/docker-compose.yml up -d
